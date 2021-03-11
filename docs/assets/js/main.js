206b7011f6cb767792276e3307375d26c205546c
Nam Nguyen Hoai <nam@icts.vn>
2021-03-11 10:07:50 +0700

    clone build to docs

---
+++ docs/assets/js/main.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(window).scroll((function(){var a=$("#header");$(window).scrollTop()>=200?a.addClass("fixed"):a.removeClass("fixed")})),$(".navbar-nav a").on("click",(function(){$(".navbar-collapse").collapse("hide")}))}));
+//# sourceMappingURL=main.js.map
